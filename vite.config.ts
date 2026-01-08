8be636b294c7f2a986f025abd9cfa5f7e5992061
Varun Soni <varun1001soni@gmail.com>
2026-01-08 16:44:02 +0530

    fix: used react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import { fileURLToPath } from 'url';
+import react from '@vitejs/plugin-react';
 
 // FIX: __dirname is not available in ES modules. This defines it using import.meta.url.
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -8,6 +9,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
